ebe2fb64118066a574344863083ea1b96916dbd3
Weliton Martins <weliton.martinsprofissional@gmail.com>
2018-07-10 20:25:05 -0300

    mapeamento endereco e class enum

---
+++ src/main/java/com/weliton/domain/Endereco.java
@@ -0,0 +1,32 @@
+package com.weliton.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ENDERECOS")
+public class Endereco extends AbstractEntity<Long> {
+	
+	@Column(nullable = false)
+	private String logradouro;
+	
+	@Column(nullable = false)
+	private String bairro;
+	
+	@Column(nullable = false)
+	private String cidade;
+	
+	
+	@Column(nullable = false, length = 2)
+	@Enumerated(EnumType.STRING)
+	private UF uf;
+	
+	@Column(nullable = false, length = 9)
+	private String cep;
+	
+	@Column(nullable = false, length = 5)
+	private Integer numero;
+}
---
+++ src/main/java/com/weliton/domain/UF.java
@@ -0,0 +1,5 @@
+package com.weliton.domain;
+
+public enum UF {
+
+}
