69f26241d71fc66eceecb4bf8a29bd36d9466b6a
Weliton Martins <weliton.martinsprofissional@gmail.com>
2018-07-10 20:44:37 -0300

    mapeamento funcuinario

--- src/main/java/com/weliton/domain/Departamento.java
+++ src/main/java/com/weliton/domain/Departamento.java
@@ -1,7 +1,10 @@
 package com.weliton.domain;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -10,7 +13,10 @@ public class Departamento extends AbstractEntity<Long> {
 	
 	@Column(name ="nome", nullable = false, unique = true, length = 60)
 	private String nome;
-
+	
+	@OneToMany(mappedBy = "departamento" )
+	private List<Cargo> cargos;
+	
 	public String getNome() {
 		return nome;
 	}
@@ -18,5 +24,14 @@ public class Departamento extends AbstractEntity<Long> {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
+	public List<Cargo> getCargos() {
+		return cargos;
+	}
+
+	public void setCargos(List<Cargo> cargos) {
+		this.cargos = cargos;
+	}
+	
 	
 }
--- src/main/java/com/weliton/domain/Endereco.java
+++ src/main/java/com/weliton/domain/Endereco.java
@@ -6,6 +6,7 @@ import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.Table;
 
+@SuppressWarnings("serial")
 @Entity
 @Table(name = "ENDERECOS")
 public class Endereco extends AbstractEntity<Long> {
@@ -29,4 +30,64 @@ public class Endereco extends AbstractEntity<Long> {
 	
 	@Column(nullable = false, length = 5)
 	private Integer numero;
+	
+	private String complemento;
+
+	public String getLogradouro() {
+		return logradouro;
+	}
+
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public UF getUf() {
+		return uf;
+	}
+
+	public void setUf(UF uf) {
+		this.uf = uf;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public Integer getNumero() {
+		return numero;
+	}
+
+	public void setNumero(Integer numero) {
+		this.numero = numero;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+	
+	
 }
