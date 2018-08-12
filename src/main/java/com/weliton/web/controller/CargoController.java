85a0285456261df416d4dcb32a85960c75244327
Weliton Martins <weliton.martinsprofissional@gmail.com>
2018-08-12 01:04:42 -0300

    criando post de cargo e configurando views

--- src/main/java/com/weliton/web/controller/CargoController.java
+++ src/main/java/com/weliton/web/controller/CargoController.java
@@ -1,15 +1,32 @@
 package com.weliton.web.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.weliton.domain.Cargo;
+import com.weliton.domain.Departamento;
+import com.weliton.service.CargoService;
+import com.weliton.service.DepartamentoService;
 
 @Controller
 @RequestMapping("/cargos")
 public class CargoController {
 	
+	@Autowired
+	private CargoService cs;
+	
+	@Autowired
+	private DepartamentoService dps;
+	
 	@GetMapping("/cadastrar")
-	public String cadastrar() {
+	public String cadastrar(Cargo cargo) {
 		return "cargo/cadastro";
 	}
 	
@@ -17,5 +34,17 @@ public class CargoController {
 	public String listar() {
 		return "cargo/lista";
 	}
+	@PostMapping("/salvar")
+	public String salvar(Cargo cargo, RedirectAttributes attr) {
+		cs.salvar(cargo);
+		attr.addFlashAttribute("success", "Cargo salvo com sucesso.");
+		return "redirect:/cargos/cadastrar";
+	}
+	
+	@ModelAttribute("departamentos")
+	public List<Departamento> listaDepartamento(){
+		return dps.BuscarTodos();
+	}
+	
 
 }
--- src/main/java/com/weliton/web/controller/DepartamentoController.java
+++ src/main/java/com/weliton/web/controller/DepartamentoController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.weliton.domain.Departamento;
 import com.weliton.service.DepartamentoService;
@@ -32,8 +33,9 @@ public class DepartamentoController {
 	}
 	
 	@PostMapping("/salvar")
-	public String salvar(Departamento departamento) {
+	public String salvar(Departamento departamento, RedirectAttributes attr) {
 		ds.salvar(departamento);
+		attr.addFlashAttribute("success", "Departamento salvo com sucesso.");
 		return "redirect:/departamentos/cadastrar";
 	}
 	
@@ -44,16 +46,20 @@ public class DepartamentoController {
 		
 	}
 	@PostMapping("/editar")
-	public String editar(Departamento departamento) {
+	public String editar(Departamento departamento, RedirectAttributes attr) {
 		ds.editar(departamento);
+		attr.addFlashAttribute("success", "Departamento editado com sucesso.");
 		return "redirect:/departamentos/cadastrar";
 		
 	}
 	@GetMapping("excluir/{id}")
 	public String excluir(@PathVariable("id")Long id, ModelMap model) {
 	
-		if(!ds.departamentoTemCargo(id)) {
+		if(ds.departamentoTemCargo(id)) {
+			model.addAttribute("fail", "Departamento não removido. Possui cargos(s) vinculados(s).");
+		}else {
 			ds.excluir(id);
+			model.addAttribute("success", "Departamento exluído com sucesso.");
 		}
 		return listar(model);
 	}
